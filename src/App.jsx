c830e90be6d378e5f4f69212ed0fafbc480404ed
Poglia <pedro.poglia@hotmail.com>
2023-03-30 23:45:58 -0300

    006 | Deleting Old Css And Change Imports on App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route exact path='/' element={<Register/>}/>
-      <Route exact path='/login' element={<Login/>}/>
-    </Routes>
-  </Router>
+      <Routes>
+        <Route exact path="/" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
