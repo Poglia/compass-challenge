a42fcc3822377ddbf9b7a3635a0eb0f7d5ee169c
Poglia <pedro.poglia@hotmail.com>
2023-04-03 15:50:33 -0300

    036 | Indent, Formatting and Documenting - Warning Component

--- src/components/react/Warning.jsx
+++ src/components/react/Warning.jsx
@@ -1,28 +1,32 @@
-import React, { useState, useEffect, useContext } from 'react';
-import {theme} from "../../styles/defaultVariables"
-import {SP2} from "../Text.style";
-import {SDivError} from "../Form.style";
+import React, { useState, useEffect, useContext } from "react";
+
+// Import Default Variables - Colors, Fonts...
+import { theme } from "../../styles/defaultVariables";
+
+// Import Component-Styles
+import { SP2 } from "../Text.style";
+import { SDivError } from "../Form.style";
 
 import AppContext from "../../AppContext";
 
-function Warning(){
-    const [heightDiv, setHeightDiv] = useState(0);    
-    const { errorMessage } = useContext(AppContext);
-    const sizeObject = Object.keys(errorMessage).length;
-    
-    // const arrMessages = errorMessage.split(".");
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(()=>{
-        setHeightDiv(sizeObject !== 0 ? (sizeObject + "vw") : 0);
-      });
+function Warning() {
+  const [heightDiv, setHeightDiv] = useState(0);
+  const { errorMessage } = useContext(AppContext);
 
-    return (
-        <SDivError style={{height: heightDiv}}>
-             {errorMessage.map((message) => (
-            <SP2 style={{color: theme.colors.textError}}>{message}</SP2>
+  const sizeObject = Object.keys(errorMessage).length;
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    setHeightDiv(sizeObject !== 0 ? sizeObject + "vw" : 0);
+  });
+
+  return (
+    <SDivError style={{ height: heightDiv }}>
+      {errorMessage.map((message) => (
+        <SP2 style={{ color: theme.colors.textError }}>{message}</SP2>
       ))}
-        </SDivError>
-    )
+    </SDivError>
+  );
 }
 
 export default Warning;
