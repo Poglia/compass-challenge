cd36698612df70b83508761d5c25db5a270c3ffb
Poglia <pedro.poglia@hotmail.com>
2023-05-02 15:40:22 -0300

    feat:: start login auth

---
+++ src/contexts/auth/AuthProvider.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=AuthProvider.js.map
---
+++ src/contexts/auth/RequireAuth.tsx
@@ -0,0 +1,13 @@
+// import React, { useContext } from "react";
+// // import Login from "../../pages/Login";
+// import { AuthContext } from "./AuthContext";
+
+// export const RequireAuth = ({ children }: { children: JSX.Element }) => {
+//     const auth = useContext(AuthContext);
+
+//     if (!auth.user) {
+//        <h1>Teste</h1>
+//     }
+
+//     return children;
+// }
